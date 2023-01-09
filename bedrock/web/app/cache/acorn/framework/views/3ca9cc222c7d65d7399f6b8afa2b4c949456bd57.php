a699f30d37be44b9df8ac2328bae1ed55bafa5df
John Lito Bardinas <bardinasjohnlito@gmail.com>
2023-01-09 11:14:19 +0800

    Updat .gitignore file

--- bedrock/web/app/cache/acorn/framework/views/3ca9cc222c7d65d7399f6b8afa2b4c949456bd57.php
+++
@@ -1,4 +0,0 @@
-<footer class="content-info">
-  <?php (dynamic_sidebar('sidebar-footer')); ?>
-</footer>
-<?php /**PATH /var/www/html/web/app/themes/ivytemplate/resources/views/sections/footer.blade.php ENDPATH**/ ?>
--- bedrock/web/app/cache/acorn/framework/views/aaa84a782dd15288f42fcda2633b57bc4ca694a2.php
+++
@@ -1,2 +0,0 @@
-<?php (dynamic_sidebar('sidebar-primary')); ?>
-<?php /**PATH /var/www/html/web/app/themes/ivytemplate/resources/views/sections/sidebar.blade.php ENDPATH**/ ?>
